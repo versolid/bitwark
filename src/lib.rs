3c81b6d4a522a9d8b15a51c0d033b4c4054edf99
Ivan <ermolaevym@gmail.com>
2023-10-08 16:50:02 +0200

    Formatted docs

--- src/lib.rs
+++ src/lib.rs
@@ -22,18 +22,25 @@
 //!     pub permissions: Vec<String>,
 //! }
 //! // Generate an EdDSA key pair with a validity period of 10 minutes and a salt with a validity of 5 minutes.
-//! let exp_key = AutoExpiring::<EdDsaKey>::generate(Duration::minutes(10)).unwrap();
-//! let exp_salt = AutoExpiring::<Salt64>::generate(Duration::minutes(5)).unwrap();
+//! let exp_key = AutoExpiring::<EdDsaKey>::generate(
+//!     Duration::minutes(10)
+//! ).unwrap();
+//! let exp_salt = AutoExpiring::<Salt64>::generate(
+//!     Duration::minutes(5)
+//! ).unwrap();
 //!
 //! // Instantiate a token with specified claims.
 //! let claims = Claims { permissions: vec!["users:read".to_string(), "users:write".to_string()]};
 //! let token = ExpiringSigned::<Claims>::new(Duration::seconds(120), claims).unwrap();
 //!
 //! // Create a binary encoding of the token, signed with the key and salt.
-//! let signed_token_bytes = token.encode_and_sign_salted(&exp_salt, &*exp_key).expect("Failed to sign token");
+//! let signed_token_bytes = token.encode_and_sign_salted(&exp_salt, &*exp_key)
+//!     .expect("Failed to sign token");
 //!
 //! // Decode the token and verify its signature and validity.
-//! let decoded_token = ExpiringSigned::<Claims>::decode_and_verify_salted(&signed_token_bytes, &exp_salt, &*exp_key).expect("Failed to decode a token");
+//! let decoded_token = ExpiringSigned::<Claims>::decode_and_verify_salted(
+//!     &signed_token_bytes, &exp_salt, &*exp_key
+//! ).expect("Failed to decode a token");
 //! assert_eq!(2, decoded_token.permissions.len(), "Failed to find 2 permissions");
 //! ```
 //! ## Key Generation and Management
